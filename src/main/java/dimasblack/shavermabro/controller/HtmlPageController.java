5ce19e21d19406457d364cadaf4ec77151950c27
DimasCoder <dimakulykovych@gmail.com>
2020-12-02 17:40:00 +0200

    Add HtmlPageController

---
+++ src/main/java/dimasblack/shavermabro/controller/HtmlPageController.java
@@ -0,0 +1,28 @@
+package dimasblack.shavermabro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HtmlPageController {
+
+    @GetMapping("/delivery")
+    public String delivery(){
+        return "index.html";
+    }
+
+    @GetMapping("/reviews")
+    public String reviews(){
+        return "index.html";
+    }
+
+    @GetMapping("/faq")
+    public String faq(){
+        return "index.html";
+    }
+
+    @GetMapping("/ordering")
+    public String ordering(){
+        return "index.html";
+    }
+}
